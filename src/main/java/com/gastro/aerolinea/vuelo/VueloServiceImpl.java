175f440b8e2e3e33cb7eac77b637a63146dd6020
Cristian Martin Quintero <lasior0177@gmail.com>
2026-04-16 11:16:24 +0100

    Corregido vuelo repository y service para poder actualizar estado y eliminar un vuelo

--- src/main/java/com/gastro/aerolinea/vuelo/VueloServiceImpl.java
+++ src/main/java/com/gastro/aerolinea/vuelo/VueloServiceImpl.java
@@ -32,4 +32,26 @@ public class VueloServiceImpl implements VueloService {
 	public List<Vuelo> buscarDisponibles() {
 		return vueloRepository.buscarDisponibles();
 	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Vuelo> findByNumeroVuelo(String numeroVuelo) {
+	    return vueloRepository.findByNumeroVuelo(numeroVuelo);
+	}
+
+	@Override
+	public void cambiarEstado(String numeroVuelo, EstadoVuelo estado) {
+	    Vuelo vuelo = vueloRepository.findByNumeroVuelo(numeroVuelo)
+	            .orElseThrow(() -> new RuntimeException("Vuelo no encontrado con numero: " + numeroVuelo));
+
+	    vuelo.setEstado(estado);
+	    vueloRepository.save(vuelo);
+	}
+	
+    public void eliminar(String numeroVuelo) {
+        Vuelo vuelo = vueloRepository.findByNumeroVuelo(numeroVuelo)
+                .orElseThrow(() -> new RuntimeException("Vuelo no encontrado con numero: " + numeroVuelo));
+
+        vueloRepository.delete(vuelo);
+    }
 }

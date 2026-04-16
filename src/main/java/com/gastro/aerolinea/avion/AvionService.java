88910b6952cfc59615391f704d319777a11efb3b
Tony Alejandro Guilarte Alvarez <gamesmaker2019@gmail.com>
2026-04-16 10:20:44 +0100

    feat: añadir el servicio de avion junto con la implementación

---
+++ src/main/java/com/gastro/aerolinea/avion/AvionService.java
@@ -0,0 +1,13 @@
+package com.gastro.aerolinea.avion;
+
+import java.util.List;
+
+public interface AvionService {
+
+    List<AvionDTO> listarTodos();
+    AvionDTO obtenerPorMatricula(String matricula);
+    AvionDTO crear(AvionDTO avionDTO);
+    AvionDTO actualizar(String matricula, AvionDTO avionDTO);
+
+    void eliminar(String matricula);
+}

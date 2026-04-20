310d4d84b732c1e83e59cca1857d79675938f234
Tony Alejandro Guilarte Alvarez <gamesmaker2019@gmail.com>
2026-04-20 12:02:16 +0100

    hotfix: arreglo parcial de los servicios del módulo avion

--- src/main/java/com/gastro/aerolinea/avion/AvionService.java
+++ src/main/java/com/gastro/aerolinea/avion/AvionService.java
@@ -1,64 +1,16 @@
 package com.gastro.aerolinea.avion;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class AvionService {
+public interface AvionService {
 
-    private final AvionRepository avionRepository;
+    List<AvionDTO> obtenerTodos();
 
-    public AvionService(AvionRepository avionRepository) {
-        this.avionRepository = avionRepository;
-    }
+    AvionDTO obtenerPorMatricula(String matricula);
 
-    public List<AvionDTO> obtenerTodos() {
-        return avionRepository.findAll()
-                .stream()
-                .map(AvionMapper::toDTO)
-                .toList();
-    }
+    AvionDTO crear(AvionDTO avionDTO);
 
-    public AvionDTO obtenerPorMatricula(String matricula) {
-        Avion avion = avionRepository.findByMatricula(matricula)
-                .orElseThrow(() -> new RuntimeException("Avión no encontrado con matrícula: " + matricula));
+    AvionDTO actualizar(String matricula, AvionDTO avionDTO);
 
-        return AvionMapper.toDTO(avion);
-    }
-
-    public AvionDTO crear(AvionDTO avionDTO) {
-        avionRepository.findByMatricula(avionDTO.getMatricula())
-                .ifPresent(a -> {
-                    throw new RuntimeException("Ya existe un avión con matrícula: " + avionDTO.getMatricula());
-                });
-
-        Avion avion = AvionMapper.toEntity(avionDTO);
-        Avion avionGuardado = avionRepository.save(avion);
-
-        return AvionMapper.toDTO(avionGuardado);
-    }
-
-    public AvionDTO actualizar(String matricula, AvionDTO avionDTO) {
-        Avion avionExistente = avionRepository.findByMatricula(matricula)
-                .orElseThrow(() -> new RuntimeException("Avión no encontrado con matrícula: " + matricula));
-
-        //avionExistente.setMatricula(avionDTO.getMatricula());
-        avionExistente.setModelo(
-                avionDTO.getModelo() != null ? ModeloAvion.fromValue(avionDTO.getModelo()) : null
-        );
-        avionExistente.setFabricante(avionDTO.getFabricante());
-        avionExistente.setCapacidadPasajeros(avionDTO.getCapacidadPasajeros());
-        avionExistente.setAnioFabricacion(avionDTO.getAnioFabricacion());
-
-        Avion avionActualizado = avionRepository.save(avionExistente);
-        return AvionMapper.toDTO(avionActualizado);
-    }
-
-    public void eliminar(String matricula) {
-        Avion avion = avionRepository.findByMatricula(matricula)
-                .orElseThrow(() -> new RuntimeException("Avión no encontrado con matrícula: " + matricula));
-
-        avionRepository.delete(avion);
-    }
+    void eliminar(String matricula);
 }

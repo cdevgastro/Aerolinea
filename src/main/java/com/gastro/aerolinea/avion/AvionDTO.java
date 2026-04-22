2747b272342d2be1dae6632d3d72a7c2d7b97c76
Tony Alejandro Guilarte Alvarez <gamesmaker2019@gmail.com>
2026-04-22 13:22:45 +0100

    fix: mejoras en avioncontroller y avionDTO + implementación de OpenAPI en pom.xml

--- src/main/java/com/gastro/aerolinea/avion/AvionDTO.java
+++ src/main/java/com/gastro/aerolinea/avion/AvionDTO.java
@@ -3,6 +3,9 @@ package com.gastro.aerolinea.avion;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
 
 @Data
 @NoArgsConstructor
@@ -10,9 +13,19 @@ import lombok.NoArgsConstructor;
 public class AvionDTO {
 
     private Long id;
+
+    @NotBlank(message = "La matrícula es obligatoria")
     private String matricula;
+    
+    @NotNull(message = "El modelo es obligatorio")
     private String modelo;
+
+    @NotBlank(message = "El fabricante es obligatorio")
     private String fabricante;
+    
+    @NotNull(message = "La capacidad de pasajeros es obligatoria")
     private int capacidadPasajeros;
+
+    @Min(value = 1900, message = "Año inválido")
     private int anioFabricacion;
 }

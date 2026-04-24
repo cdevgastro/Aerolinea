33be45f3c747e09fa6dcdf265c74c6f34adcf3c0
= <=>
2026-04-24 12:32:24 +0100

    Arreglao con errores

--- src/main/java/com/gastro/aerolinea/avion/AvionDTO.java
+++ src/main/java/com/gastro/aerolinea/avion/AvionDTO.java
@@ -1,8 +1,5 @@
 package com.gastro.aerolinea.avion;
 
-import com.gastro.aerolinea.reserva.Reserva;
-import com.gastro.aerolinea.reserva.mapper.ReservaDto;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,24 +12,6 @@ import jakarta.validation.constraints.Min;
 @AllArgsConstructor
 public class AvionDTO {
 
-    public static Reserva toEntity(ReservaDto dto) {
-        if (dto == null) {
-            return null;
-        }
-        Reserva reserva = new Reserva();
-        reserva.setIdReserva(dto.getIdReserva());
-        reserva.setNumeroVuelo(dto.getNumeroVuelo());
-        reserva.setOrigen(dto.getOrigen());
-        reserva.setDestino(dto.getDestino());
-        reserva.setFechaSalida(dto.getFechaSalida());
-        reserva.setFechaLlegada(dto.getFechaLlegada());
-        reserva.setNombrePasajero(dto.getNombrePasajero());
-        reserva.setDniPasajero(dto.getDniPasajero());
-        reserva.setAsiento(dto.getAsiento());
-        reserva.setPrecio(dto.getPrecio());
-        reserva.setEstado(dto.getEstado());
-        return reserva;
-    }
     private Long id;
 
     @NotBlank(message = "La matrícula es obligatoria")

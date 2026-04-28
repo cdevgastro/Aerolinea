d1b2de57333bf20730f3ff6fd9df2a456f1f42f4
Tony Alejandro Guilarte Alvarez <gamesmaker2019@gmail.com>
2026-04-28 12:18:03 +0100

    fix/Añadido MockitoBean en AvionControllerTest y en VueloControllerTest

--- src/test/java/com/gastro/aerolinea/avion/AvionControllerTest.java
+++ src/test/java/com/gastro/aerolinea/avion/AvionControllerTest.java
@@ -22,7 +22,7 @@ class AvionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-
+    @MockitoBean
     private AvionService avionService;
 
     @Autowired
--- src/test/java/com/gastro/aerolinea/vuelo/VueloControllerTest.java
+++ src/test/java/com/gastro/aerolinea/vuelo/VueloControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -23,6 +24,7 @@ class VueloControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
     private VueloService vueloService;
 
     @Autowired
